37a0e2df16146f41ff579d9fd2d62e124e0f1365
yuna <yuna2101@naver.com>
2024-07-26 14:41:22 +0900

    board_list update

--- NBTI-FrontEnd/src/components/Body/SubPage/Board/Content/List/List.jsx
+++ NBTI-FrontEnd/src/components/Body/SubPage/Board/Content/List/List.jsx
@@ -1,30 +1,24 @@
 import styles from "./List.module.css";
 
+
 export const List = () => {
     return (
         <div className={styles.container}>
             <h1>자유 게시판</h1>
             <div className={styles.searchBox}>
+                <div className={styles.dropdown}>
+                    <select>
+                        <option value="title">제목</option>
+                        <option value="content">내용</option>
+                        <option value="writer">작성자</option>
+                    </select>
+                </div>
                 <input type="text" placeholder="검색" />
                 <button>검색</button>
             </div>
             <div className={styles.list}></div>
 
-
-
-            {/* <div className={styles.header}>
-                <h1>자유게시판 </h1>
-                <div className={styles.searchBox}>
-                    <div></div>
-                    <div>
-
-                    </div>
-                </div>
-            </div>
-            <div className={styles.body}></div> */}
-
         </div>
     )
-
 
 }
