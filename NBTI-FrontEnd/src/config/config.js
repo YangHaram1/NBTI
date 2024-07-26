b80f6524ebe0c6cba14fd332e974229c398c9bc6
YANGHARAM <apdlvmf33@naver.com>
2024-07-26 10:13:41 +0900

    setup

--- NBTI-FrontEnd/src/config/config.js
+++ NBTI-FrontEnd/src/config/config.js
@@ -1,2 +1,2 @@
 export const host="192.168.1.179:80";
-export const api="9bewfouem96jjnfvuu236yinb3kp53xruh2gkkz3pkfnkw6c"; //tinymce     
+export const api="9bewfouem96jjnfvuu236yinb3kp53xruh2gkkz3pkfnkw6c"; //tinymce
