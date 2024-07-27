34362970f9f52d1724da3edfc0c9b7c13fc31e72
YANGHARAM <apdlvmf33@naver.com>
2024-07-27 20:49:38 +0900

    update group_chat

--- NBTI-BackEnd/src/main/java/com/nbti/dao/Group_chatDAO.java
+++ NBTI-BackEnd/src/main/java/com/nbti/dao/Group_chatDAO.java
@@ -1,8 +1,20 @@
 package com.nbti.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.nbti.dto.Group_chatDTO;
 
 @Repository
 public class Group_chatDAO {
-
+	@Autowired
+	private SqlSession mybatis;
+	
+	public int insert(String member_id ) throws Exception{
+		Group_chatDTO dto =new Group_chatDTO();
+		dto.setName(member_id);
+		mybatis.insert("Group_chat.insert",dto);
+		return dto.getSeq();
+	}
 }
---
+++ NBTI-BackEnd/src/main/java/com/nbti/dao/Group_memberDAO.java
@@ -0,0 +1,17 @@
+package com.nbti.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.nbti.dto.Group_memberDTO;
+
+@Repository
+public class Group_memberDAO {
+	@Autowired
+	private SqlSession mybatis;
+	
+	public void insert(Group_memberDTO dto) throws Exception {
+		mybatis.insert(null);
+	}
+}
