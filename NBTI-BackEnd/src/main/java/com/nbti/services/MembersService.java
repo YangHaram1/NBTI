7e325586bfd8bc21b73103dff3d12db9f61a96d9
SSH <kps10011@naver.com>
2024-07-29 15:21:52 +0900

    store.js modified

--- NBTI-BackEnd/src/main/java/com/nbti/services/MembersService.java
+++ NBTI-BackEnd/src/main/java/com/nbti/services/MembersService.java
@@ -1,10 +1,12 @@
 package com.nbti.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.nbti.dto.MembersDTO;
 import com.nbti.dao.MembersDAO;
+import com.nbti.dto.MembersDTO;
 
 @Service
 public class MembersService {
@@ -24,4 +26,8 @@ public class MembersService {
 		mdao.updateMyData(dto);
 	}
 	
+	public List<MembersDTO> selectAll(){
+		return mdao.selectAll();
+	}
+		
 }
