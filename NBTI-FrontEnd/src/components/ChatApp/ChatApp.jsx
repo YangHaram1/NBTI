45b75fa8fbe1a71a740cfdfc1768f74679d126ef
YANGHARAM <apdlvmf33@naver.com>
2024-07-30 11:56:16 +0900

    update alarm

--- NBTI-FrontEnd/src/components/ChatApp/ChatApp.jsx
+++ NBTI-FrontEnd/src/components/ChatApp/ChatApp.jsx
@@ -10,6 +10,17 @@ import { useContext } from 'react';
 axios.defaults.withCredentials = true;
 
 const ChatApp = () => {
+    if ("Notification" in window) {
+        Notification.requestPermission().then(permission => {
+          if (permission === "granted") {
+            console.log("Notification permission granted.");
+          } else if (permission === "denied") {
+            console.log("Notification permission denied.");
+          } else {
+            console.log("Notification permission default.");
+          }
+        });
+      }
     //   <Outlet />
     const { chatAppRef,chatNavi} = useContext(ChatsContext);
     if(chatNavi===''){
