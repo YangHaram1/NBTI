7196a910f76f3799c0646b7347b15cf2d6d8a37b
ol7mi <dmsal9865@gmail.com>
2024-08-02 03:32:04 +0900

    캘린더 추가 기능

--- NBTI-FrontEnd/src/components/Body/SubPage/Calendar/Calendar.jsx
+++ NBTI-FrontEnd/src/components/Body/SubPage/Calendar/Calendar.jsx
@@ -5,11 +5,13 @@ import { Side } from './Side/Side';
 
 export const Calendar = () => {
     
-  const [addOpen, setAddOpen] = useState(false);
+  const [addOpen, setAddOpen] = useState(false); //일정 모달 상태 
+  const [calendarModalOpen, setCalendarModalOpen] = useState(false); // 추가 모달 상태
+
     return(
         <div className={styles.container}>
-            <Side setAddOpen={setAddOpen}/>
-            <Content addOpen={addOpen} setAddOpen={setAddOpen} />
+            <Side setAddOpen={setAddOpen} setCalendarModalOpen={setCalendarModalOpen} />
+            <Content addOpen={addOpen} setAddOpen={setAddOpen} setCalendarModalOpen={setCalendarModalOpen} calendarModalOpen={calendarModalOpen}/>
         </div>
     )
 };
