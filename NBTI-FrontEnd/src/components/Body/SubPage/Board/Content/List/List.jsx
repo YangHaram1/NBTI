99f783da5ddf07bd65f51140480bef3c13820158
yuna <yuna2101@naver.com>
2024-08-01 17:19:23 +0900

    board search

--- NBTI-FrontEnd/src/components/Body/SubPage/Board/Content/List/List.jsx
+++ NBTI-FrontEnd/src/components/Body/SubPage/Board/Content/List/List.jsx
@@ -8,30 +8,47 @@ import { host } from "../../../../../../config/config";
 
 export const List = () => {
     const navi = useNavigate();
-    const [boardList, setBoardList] = useState([{}]);
+    const [boardList, setBoardList] = useState([]);
     const { boardType, setBoardSeq } = useBoardStore();
+    const [target, setTarget] = useState('');  // target 초기 값을 빈 문자열로 설정
+    const [keyword, setKeyword] = useState('');     // keyword 초기 값
+    const [start, setStart] = useState(0); // 페이지네이션 시작 인덱스
+    const [end, setEnd] = useState(100); // 페이지네이션 종료 인덱스
+    const [search, setSearch] = useState(false);
+
 
     // 1 : 자유 2: 공지
     let code = 1;
     if (boardType === "자유") code = 1;
     else if (boardType === "공지") code = 2;
 
+
+    // 게시판 목록 출력 
+    const params = { code: code, target: target, keyword: keyword, start: start, end: end };
     useEffect(() => {
-        axios.get(`${host}/board/${code}`).then((resp) => {
+        axios.get(`${host}/board/list`, { params }).then((resp) => {
             console.log("게시판 : " + resp.data);
             setBoardList(resp.data);
         });
-    }, [boardType]);
+    }, [boardType, search]);
+
+    // 검색 버튼 클릭 핸들러
+    const handleSearch = () => {
+        setSearch((prev) => {
+            return !prev;
+        })
+        // setKeyword('');
+    };
 
 
-    // 조회수 증가 
+
+    // 조회수 증가 및 상세 페이지로 이동 
     const handleTitleClick = (seq) => {
         const requestBox = { seq: seq, board_code: code };
         axios.put(`${host}/board/viewCount`, requestBox).then((resp) => {
             setBoardSeq(seq);
             navi("/board/detail");
         });
-
     };
 
     // 페이지네이션
@@ -42,14 +59,20 @@ export const List = () => {
             <h1>{boardType} 게시판</h1>
             <div className={styles.searchBox}>
                 <div className={styles.dropdown}>
-                    <select>
+                    <select value={target} onChange={(e) => setTarget(e.target.value)}>
+                        <option value="">전체</option>
                         <option value="title">제목</option>
-                        <option value="content">내용</option>
-                        <option value="writer">작성자</option>
+                        <option value="contents">내용</option>
+                        <option value="member_id">작성자</option>
                     </select>
                 </div>
-                <input type="text" placeholder="검색" />
-                <button>검색</button>
+                <input
+                    type="text"
+                    placeholder="검색"
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                />
+                <button onClick={handleSearch}>검색</button>
             </div>
             <div className={styles.body}>
                 <div className={styles.listTitle}>
