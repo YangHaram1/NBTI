31ae0112d84a105ea165ec61652692d6f7d472d2
kjy <kimjy2718@gmail.com>
2024-08-02 11:24:42 +0900

    approval write back

--- NBTI-BackEnd/src/main/java/com/nbti/dao/ApprovalLineDAO.java
+++ NBTI-BackEnd/src/main/java/com/nbti/dao/ApprovalLineDAO.java
@@ -4,10 +4,16 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.nbti.dto.ApprovalLineDTO;
+
 @Repository
 public class ApprovalLineDAO {
 	
 	@Autowired
 	private SqlSession mybatis;
+	
+	public void insert(ApprovalLineDTO dto) {
+		mybatis.insert("ApprovalLine.insert", dto);
+	}
 
 }
