9b17c7516b2acea8f480a99d2d2aec1917a1539d
YANGHARAM <apdlvmf33@naver.com>
2024-08-04 22:22:31 +0900

    update useradmin

--- NBTI-FrontEnd/src/components/Security/Security.jsx
+++ NBTI-FrontEnd/src/components/Security/Security.jsx
@@ -1,5 +1,10 @@
+import styles from './Security.module.css';
 const Security =()=>{
+    return (
+        <div className={styles.container}>
+            security
+        </div>
 
-
+    );
 }
 export default Security;
