dd313fa047f8bbe3b771f5768c96dffdb68657f5
YANGHARAM <apdlvmf33@naver.com>
2024-08-04 22:43:59 +0900

    update user_history

--- NBTI-FrontEnd/src/components/Security/Security.jsx
+++ NBTI-FrontEnd/src/components/Security/Security.jsx
@@ -1,8 +1,42 @@
 import styles from './Security.module.css';
-const Security =()=>{
+import { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
+import { host } from '../../config/config';
+const Security = () => {
+    const [history, setHistory] = useState([]);
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${host}/user_history`).then((resp) => {
+
+        })
+    }, [])
+
+    const handleList = useCallback(() => {
+        setList(history);
+    }, [history])
+
     return (
         <div className={styles.container}>
-            security
+            <div className={styles.header}>
+                <div>
+                    seq
+                </div>
+                <div>
+                    User_ID
+                </div>
+                <div>
+                    IP
+                </div>
+                <div>
+                    접속시간
+                </div>
+            </div>
+            <div className={styles.list}>
+                {
+                    list
+                }
+            </div>
         </div>
 
     );
