ee2a191472bc0201709feb33203f18cd0c1b9b05
YANGHARAM <apdlvmf33@naver.com>
2024-08-05 10:45:39 +0900

    update user_history page

--- NBTI-FrontEnd/src/components/Security/Security.jsx
+++ NBTI-FrontEnd/src/components/Security/Security.jsx
@@ -6,50 +6,46 @@ import { format } from 'date-fns';
 import ReactPaginate from 'react-paginate';
 const Security = () => {
     const [history, setHistory] = useState([]);
-    const [list, setList] = useState([]);
-    const [page,setPage]=useState(1);
+    const [cpage,setCpage]=useState(1);
     const [page_total_count,setPage_total_count] =useState();
-    const record_count_per_page=10;
-    const navi_count_per_page=4;
+    const [target,setTarget] =useState('');
+    const [keyword,setKeyword]=useState('');
+    const record_count_per_page=25;
+    const navi_count_per_page=5;
+ 
 
     useEffect(() => {
-        axios.get(`${host}/user_history`).then((resp) => {
-            setHistory(resp.data);
-
+        const start=cpage*record_count_per_page -(record_count_per_page-1); //1
+        const end=cpage*record_count_per_page; //10
+        axios.get(`${host}/user_history?start=${start}&&end=${end}&&target=${target}&&keyword=${keyword}`).then((resp) => {
+            console.log(resp.data)
+            setHistory((prev)=>{
+                const record_total_count=resp.data.count;
+                if(record_total_count%record_count_per_page===0){
+                    setPage_total_count(Math.floor(record_total_count/record_count_per_page));
+                }
+                else{
+                    setPage_total_count(Math.floor(record_total_count/record_count_per_page)+1);
+                }
+                return resp.data.list;
+            });
         })
-    }, [])
-
-    const handleList = useCallback(() => {
-        setList(history);
-        
-    }, [history])
-
-    useEffect(() => {
-        if (history.length > 0) {
-            handleList();
-        }
-    }, [history])
-
+    }, [cpage])
 
     const handlePage=(selectedPage)=>{
-        setPage(selectedPage.selected);
+        setCpage(selectedPage.selected+1);
     }
 
     const pageSetting=useCallback(()=>{
-        const record_total_count =list.length;
-        if(record_total_count%record_count_per_page===0){
-			setPage_total_count(Math.floor(record_total_count/record_count_per_page));
-		}
-		else{
-			setPage_total_count(Math.floor(record_total_count/record_count_per_page)+1);
-		}
-    },[list])
+       
+    },[history])
 
     useEffect(()=>{
-        if(list.length>0){
+        if(history.length>0){
             pageSetting();
         }
     },[pageSetting])
+
 
     return (
         <div className={styles.container}>
@@ -72,8 +68,8 @@ const Security = () => {
             </div>
             <div className={styles.list}>
                 {
-                    list.map((item, index) => {
-                        const currentDate = format(new Date(item.join_date), 'yyyy-MM-dd HH-mm-ss');
+                    history.map((item, index) => {
+                        const currentDate = format(new Date(item.join_date), 'yyyy-MM-dd HH:mm:ss');
                         const date=currentDate.split(" ");
                         return (
                             <div className={styles.item} key={index}>
@@ -99,8 +95,8 @@ const Security = () => {
             </div>
             <ReactPaginate
                 pageCount={page_total_count} // 페이지 수
-                pageRangeDisplayed={5} // 현재 페이지를 기준으로 표시할 페이지 범위 수
-                marginPagesDisplayed={0} // 양쪽 끝에 표시할 페이지 수
+                pageRangeDisplayed={navi_count_per_page} // 현재 페이지를 기준으로 표시할 페이지 범위 수
+                marginPagesDisplayed={1} // 양쪽 끝에 표시할 페이지 수
                 onPageChange={handlePage} // 페이지 변경 핸들러
                 containerClassName={styles.pagination} // 스타일 클래스
                 activeClassName={styles.active} // 활성 페이지 클래스
@@ -109,7 +105,7 @@ const Security = () => {
                 previousClassName={styles.previous} // 이전 버튼의 클래스명
                 nextLabel={'>'} // 다음 페이지 버튼 레이블
                 nextClassName={styles.next} // 다음 버튼의 클래스명
-                breakLabel={null} // 생략 표시 제거
+                breakLabel={'...'} // 생략 표시 제거
                 breakClassName={null} // 생략 표시의 클래스명 제거
             />
         </div>
