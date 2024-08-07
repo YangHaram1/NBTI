73b1fffe5db914a60d239d6ff4323c1903fc4750
YANGHARAM <apdlvmf33@naver.com>
2024-08-07 18:42:55 +0900

    update group

--- NBTI-BackEnd/src/main/java/com/nbti/dto/GroupDTO.java
+++ NBTI-BackEnd/src/main/java/com/nbti/dto/GroupDTO.java
@@ -4,6 +4,7 @@ public class GroupDTO {
 	private String name;
 	private String job_name;
 	private String member_img;
+	private String id;
 	public String getName() {
 		return name;
 	}
@@ -22,15 +23,23 @@ public class GroupDTO {
 	public void setMember_img(String member_img) {
 		this.member_img = member_img;
 	}
-	public GroupDTO(String name, String job_name, String member_img) {
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public GroupDTO(String name, String job_name, String member_img, String id) {
 		super();
 		this.name = name;
 		this.job_name = job_name;
 		this.member_img = member_img;
+		this.id = id;
 	}
 	public GroupDTO() {
 		super();
 	}
+	
 	
 	
 
