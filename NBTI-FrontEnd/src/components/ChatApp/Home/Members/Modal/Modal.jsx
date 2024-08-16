7b35835c8608ab2c61e924192f3a2335aef92a5c
YANGHARAM <apdlvmf33@naver.com>
2024-08-16 13:41:51 +0900

    update modal

--- NBTI-FrontEnd/src/components/ChatApp/Home/Members/Modal/Modal.jsx
+++ NBTI-FrontEnd/src/components/ChatApp/Home/Members/Modal/Modal.jsx
@@ -5,42 +5,44 @@ import React from 'react';
 import { useState, useContext } from 'react';
 import { ChatsContext } from '../../../../../Context/ChatsContext';
 import { useAuthStore } from './../../../../../store/store';
-const Modal=({modalRef,index,item,profileRef})=>{
+const Modal = ({ modalRef, index, item, profileRef }) => {
     const [modalDisplay, setModalDisplay] = useState(null);
-    const {setChatNaviBody} =useContext(ChatsContext);
-    const {loginID} =useAuthStore();
+    const { setChatNaviBody, dragRef } = useContext(ChatsContext);
+    const { loginID } = useAuthStore();
 
-    const handleChatRoom=()=>{
+    const handleChatRoom = () => {
         //console.log(item)
-        axios.post(`${host}/group_chat?member_id=${item.id}&&name=${item.name}`).then((resp)=>{
+        axios.post(`${host}/group_chat?member_id=${item.id}&&name=${item.name}`).then((resp) => {
             setChatNaviBody("chats");
         })
     }
-    const handleMemberIfo=(index) => (e) =>{
-        const { clientX: x, clientY: y } = e;
-         e.preventDefault();
-         setModalDisplay((prev) => {
-             if (prev != null) {
-                 prev.style.display = 'none'
-             }
-             profileRef.current[index].style.display = 'flex';
-         //   profileRef.current[index].style.top = y + 'px';
-          //  profileRef.current[index].style.left = x;
-             return profileRef.current[index];
-         });
+    const handleMemberIfo = (index) => (e) => {
+        const rect = dragRef.current.getBoundingClientRect(); //부모요소~ 드래그 되는애
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        e.preventDefault();
+        setModalDisplay((prev) => {
+            if (prev != null) {
+                prev.style.display = 'none'
+            }
+            profileRef.current[index].style.display = 'flex';
+            profileRef.current[index].style.top = y + 'px';
+            profileRef.current[index].style.left = x +'px';
+            return profileRef.current[index];
+        });
     }
 
-  
 
-    return(
-        <div className={styles.container} ref={el=>modalRef.current[index]=el}>
+
+    return (
+        <div className={styles.container} ref={el => modalRef.current[index] = el}>
             <div className={styles.content} onClick={handleChatRoom}>
-                {(loginID===item.id)?'나와의 채팅하기':'채팅 하기'}
+                {(loginID === item.id) ? '나와의 채팅하기' : '채팅 하기'}
             </div>
             <div className={styles.content} onClick={handleMemberIfo(index)}>
                 사용자 정보
             </div>
-           
+
         </div>
 
 
