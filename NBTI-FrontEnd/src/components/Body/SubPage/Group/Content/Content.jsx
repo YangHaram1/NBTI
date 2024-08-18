9ed18c36b196362b9c3a07dec4ed742ae3dd6d0e
YANGHARAM <apdlvmf33@naver.com>
2024-08-18 14:44:58 +0900

    update group

--- NBTI-FrontEnd/src/components/Body/SubPage/Group/Content/Content.jsx
+++ NBTI-FrontEnd/src/components/Body/SubPage/Group/Content/Content.jsx
@@ -1,29 +1,102 @@
 import styles from './Content.module.css';
-import { useEffect, useState } from 'react';
 import { host } from '../../../../../config/config';
 import avatar from '../../../../../images/user.jpg'
-const Content = ({ member, name }) => {
-    
+import React from "react";
 
+const Content = ({list ,listDisplay,teamDisplay,mainDisplay}) => {
     return (
-        <div className={styles.container}>
-            <div className={styles.member}>
-                <div className={styles.div1}>
-                    <img src={(member.member_img===null)?`${avatar}`:`${host}/images/avatar/${member.id}/${member.member_img}`} alt="" />
-                </div>
-                <div className={styles.div2}>
-                    <div>
-                        {member.name}
-                    </div>
-                    <div>
-                        {name}
-                    </div>
-                    <div>
-                        {member.job_name}
-                    </div>
-                </div>
+        <React.Fragment>
+            <div className={styles.container}>
+                {mainDisplay && (<div className={styles.menus}>
+                    {
+                        list.map((dItem, dindex) => {
+                            const teams = dItem.teams;
+                            return (
+                                <div className={styles.menu} key={dindex} >
+
+                                    {listDisplay[dindex] && (
+                                        <div className={styles.teams}>
+                                            {
+                                                teams.map((team, tindex) => {
+                                                    const members = team.members
+                                                    return (
+                                                        <React.Fragment key={tindex}>
+                                                            <div style={{ display: "flex", flexDirection: "column" }}>
+                                                                {teamDisplay[dindex][tindex] && (<div className={styles.members}>
+                                                                    {members.length > 0 && (
+                                                                        <div>
+                                                                            {team.name}
+                                                                        </div>
+                                                                    )
+                                                                    }
+                                                                    <div className={styles.mContainer}>
+                                                                        {
+                                                                            members.slice(0, 5).map((member, mindex) => {
+                                                                                return (
+                                                                                    <React.Fragment key={mindex}  >
+
+                                                                                        <div className={styles.member}>
+                                                                                            <div className={styles.div1}>
+                                                                                                <img src={(member.member_img === null) ? `${avatar}` : `${host}/images/avatar/${member.id}/${member.member_img}`} alt="" />
+                                                                                            </div>
+                                                                                            <div className={styles.div2}>
+                                                                                                <div>
+                                                                                                    {member.name}
+                                                                                                </div>
+                                                                                        
+                                                                                                <div>
+                                                                                                    {member.job_name}
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </React.Fragment>
+                                                                                );
+
+                                                                            })
+                                                                        }
+                                                                    </div>
+                                                                    <div className={styles.mContainer}>
+                                                                        {
+                                                                            members.slice(5).map((member, mindex) => {
+                                                                                return (
+                                                                                    <React.Fragment key={mindex}  >
+
+                                                                                        <div className={styles.member}>
+                                                                                            <div className={styles.div1}>
+                                                                                                <img src={(member.member_img === null) ? `${avatar}` : `${host}/images/avatar/${member.id}/${member.member_img}`} alt="" />
+                                                                                            </div>
+                                                                                            <div className={styles.div2}>
+                                                                                                <div>
+                                                                                                    {member.name}
+                                                                                                </div>
+                                                                                        
+                                                                                                <div>
+                                                                                                    {member.job_name}
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </React.Fragment>
+                                                                                );
+
+                                                                            })
+                                                                        }
+                                                                    </div>
+                                                                </div>)}
+                                                            </div>
+                                                        </React.Fragment>
+                                                    );
+                                                })
+                                            }
+                                        </div>)}
+                                </div>
+                            );
+                        })
+                    }
+                </div>)}
             </div>
-        </div>
+        </React.Fragment>
     );
+
+
 }
 export default Content;
--- NBTI-FrontEnd/src/components/Body/SubPage/Group/Group.jsx
+++ NBTI-FrontEnd/src/components/Body/SubPage/Group/Group.jsx
@@ -1,12 +1,41 @@
 import styles from './Group.module.css';
-import { useNavigate } from 'react-router-dom';
 import  Side  from './Side/Side';
 import Content from './Content/Content' 
+import { useState,useEffect } from 'react';
+import { host } from '../../../../config/config';
+import axios from "axios";
 const Group=()=>{
-    const navi=useNavigate();
+    const [list, setList] = useState([]);
+    const [listDisplay, setListDisplay] = useState([]);
+    const [teamDisplay, setTeamDisplay] = useState([[]]);
+    const [mainDisplay, setMainDisplay] = useState(true);
+
+    useEffect(() => {
+        axios.get(`${host}/group`).then((resp) => {
+            setList(resp.data);
+            setListDisplay(() => {
+                return (
+                    resp.data.map((item, index) => {
+                        return true;
+                    })
+                );
+            })
+
+            const initialDisplayState = resp.data.map(dept =>
+                dept.teams.map(() => true
+                )
+            );
+            setTeamDisplay(initialDisplayState);
+        })
+    }, [])
+
+
+
+
     return(
         <div className={styles.container}>
-            <Side></Side>
+            <Side  list={list}  listDisplay={listDisplay} teamDisplay={teamDisplay} mainDisplay={mainDisplay} setList={setList} setListDisplay={setListDisplay} setTeamDisplay={setTeamDisplay} setMainDisplay={setMainDisplay}></Side>
+            <Content list={list}  listDisplay={listDisplay} teamDisplay={teamDisplay} mainDisplay={mainDisplay} ></Content>
         </div>
     );
 }
