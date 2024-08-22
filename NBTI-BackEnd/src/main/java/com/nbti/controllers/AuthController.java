b09869e3727d2a239b1f5f92c2c95c5fa0301dd9
YANGHARAM <apdlvmf33@naver.com>
2024-08-22 12:22:12 +0900

    ip 저장 추가

--- NBTI-BackEnd/src/main/java/com/nbti/controllers/AuthController.java
+++ NBTI-BackEnd/src/main/java/com/nbti/controllers/AuthController.java
@@ -29,75 +29,70 @@ import jakarta.servlet.http.HttpSession;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-	
-	
-	
-	//private MembersService mServ;
+
+	// private MembersService mServ;
 	@Autowired
 	private HttpSession session;
 	@Autowired
 	private HttpServletRequest request;
-	
+
 	@Autowired
 	private MembersService mServ;
 	@Autowired
 	private User_historyService uServ;
-	
-	
-	
-		
-	
-	
-	
-	
-	
+
 	@PostMapping
-	public ResponseEntity<String> login(@RequestBody Map<String, String> maps, HttpServletResponse response) throws Exception {
-	    String encryptedPassword = EncryptionUtils.getSHA512(maps.get("pw"));
-	    MembersDTO dto = new MembersDTO();
-	    dto.setPw(encryptedPassword);
-	    dto.setId(maps.get("id"));
-	    
-	    boolean result = mServ.login(dto);   
-	    if (!result) {
-	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("login Failed");
-	    }
+	public ResponseEntity<String> login(@RequestBody Map<String, String> maps, HttpServletResponse response)
+			throws Exception {
+		String encryptedPassword = EncryptionUtils.getSHA512(maps.get("pw"));
+		MembersDTO dto = new MembersDTO();
+		dto.setPw(encryptedPassword);
+		dto.setId(maps.get("id"));
 
-	    Cookie cookie = new Cookie("SESSIONID", dto.getId());
-	    cookie.setPath("/");
-	    cookie.setHttpOnly(true); // JavaScript에서 접근 불가
-	    cookie.setSecure(true); // HTTPS에서만 전송
-	    cookie.setMaxAge(3600); // 쿠키 유효 기간 설정 (1시간)
-
-	    response.addCookie(cookie); // 응답에 쿠키를 추가
-	    
-	    HttpSession session = request.getSession(); // HttpSession 객체 생성
-	    
-	    session.setAttribute("loginID", dto.getId()); // 로그인한 아이디의 세션을 담아둔다
-	    String id = (String)session.getAttribute("loginID");
-	    System.out.println("loginID : " + id);
-	    return ResponseEntity.ok(dto.getId()); // 로그인에 성공한 아이디를 돌려보낸다
-	}
-	
-	   @GetMapping
-	   public ResponseEntity<List<ChatDTO>> get(){
-			
-		   //System.out.println(session.getAttribute("loginID"));
-		   ResponseEntity.status(HttpStatus.UNAUTHORIZED);
-		   return ResponseEntity.ok(null);
-	   }
-	
-	   //로그아웃
-	   @DeleteMapping
-	   public ResponseEntity<Void> logout(){
-		   System.out.println("로그아웃 확인");
-		   session.invalidate();
-		   return ResponseEntity.ok().build();
-	   }
-	   
-	   @ExceptionHandler(Exception.class)
-		public String exceptionHandler(Exception e) {
-			e.printStackTrace();
-			return "error";
+		boolean result = mServ.login(dto);
+		if (!result) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("login Failed");
 		}
+
+		Cookie cookie = new Cookie("SESSIONID", dto.getId());
+//	    cookie.setPath("/");
+//	    cookie.setHttpOnly(true); // JavaScript에서 접근 불가
+//	    cookie.setSecure(true); // HTTPS에서만 전송
+//	    cookie.setMaxAge(3600); // 쿠키 유효 기간 설정 (1시간)
+//
+//	    response.addCookie(cookie); // 응답에 쿠키를 추가
+		String ip = request.getHeader("X-FORWARDED-FOR");
+		if (ip == null || ip.isEmpty()) {
+			ip = request.getRemoteAddr();
+		}
+		uServ.insert(new User_historyDTO(0, dto.getId(), ip, null));
+		HttpSession session = request.getSession(); // HttpSession 객체 생성
+
+		session.setAttribute("loginID", dto.getId()); // 로그인한 아이디의 세션을 담아둔다
+		String id = (String) session.getAttribute("loginID");
+		System.out.println("loginID : " + id);
+		return ResponseEntity.ok(dto.getId()); // 로그인에 성공한 아이디를 돌려보낸다
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ChatDTO>> get() {
+
+		// System.out.println(session.getAttribute("loginID"));
+		ResponseEntity.status(HttpStatus.UNAUTHORIZED);
+		return ResponseEntity.ok(null);
+	}
+
+	// 로그아웃
+	@DeleteMapping
+	public ResponseEntity<Void> logout() {
+		System.out.println("로그아웃 확인");
+		session.invalidate();
+		return ResponseEntity.ok().build();
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e) {
+		e.printStackTrace();
+		return "error";
+	}
 }
